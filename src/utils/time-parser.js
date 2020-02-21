44b413dfac0051905299e8fd79706c47233559c8
Vladimir Osokin <osokin.home@gmail.com>
2020-02-21 10:31:07 +0500

    parseTime fixed

--- src/utils/time-parser.js
+++ src/utils/time-parser.js
@@ -1,7 +1,9 @@
-export const parseTime = str => {
+const parser = str => {
   const [hours, minutes] = str.split(':').map(n => parseInt(n));
   return hours * 60 + minutes;
 };
-export const FROM = parseTime('0:00');
-export const TO = parseTime('6:00');
+export const FROM = parser('0:30');
+export const TO = parser('6:30');
 export const DURATION = TO - FROM;
+
+export const parseTime = str => parser(str) - FROM;
