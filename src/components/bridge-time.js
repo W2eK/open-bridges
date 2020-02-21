8b36cdcac5f70b65195a1487e5b2de13de24c7ce
Vladimir Osokin <osokin.home@gmail.com>
2020-02-21 12:26:58 +0500

    BridgeTime changed

--- src/components/bridge-time.js
+++ src/components/bridge-time.js
@@ -1,33 +1,20 @@
 import React from 'react'
-
 import classNames from '../styles/bridge-time.module.css';
 
 const BridgeTime = props => {
   const { segments, schedule, moveable } = props;
-  const labels = [...schedule.slice(0, 2), '', ...schedule.slice(2)]
-  const middle = segments[2];
-  const flexValues = segments.length <= 3
-  ? segments.map(() => 1).filter((n, i) => i !== 2)
-  : segments.map((n, i) => {
-    switch (i) {
-      case 2:
-        return middle * .5
-      case 1:
-      case 3:
-        return n + middle * .75
-      default:
-        return n
-    }
-  })
-  return (
-    <div className={classNames.bridgeTime}>
-      {
-        !moveable
-        ? <span>не разводной</span>
-        : flexValues.map((segment, i) => <span key={labels[i]} style={{flex: segment}}>{labels[i]}</span>)
-      }
-    </div>
+  console.log(segments)
+  const middle = segments[2] / 2;
+  const flexes = [segments[0] + segments[1] + middle, segments[3] + segments[4] + middle];
+  const spans = !moveable
+  ? <span>не разводной</span>
+  : schedule.filter((x, i) => i % 2 === 1).map((time, i) =>
+    <span key={i} style={{flex: flexes[i]}}>
+      <span>{schedule[i * 2]}</span>
+      <span>{time}</span>
+    </span>
   )
+  return <div className={classNames.bridgeTime}>{spans}</div>
 }
 
 export default BridgeTime;
--- src/utils/time-parser.js
+++ src/utils/time-parser.js
@@ -2,8 +2,9 @@ const parser = str => {
   const [hours, minutes] = str.split(':').map(n => parseInt(n));
   return hours * 60 + minutes;
 };
-export const FROM = parser('0:30');
-export const TO = parser('6:30');
-export const DURATION = TO - FROM;
+const FROM = parser('0:30');
+const TO = parser('6:30');
 
+export const DURATION = TO - FROM;
 export const parseTime = str => parser(str) - FROM;
+export const scaler = t => t / DURATION
